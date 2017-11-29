3028d1d172a442dde3deb33f10413adc4733de8a
vatri <boris.trivic@yandex.ru>
2017-11-29 16:29:33 +0100

    Added message entity and DB table for conversations

---
+++ src/main/java/net/vatri/freelanceplatform/models/Message.java
@@ -0,0 +1,84 @@
+package net.vatri.freelanceplatform.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Message {
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_sender", nullable = false)
+    private User userSender;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_receiver", nullable = false)
+    private User userReceiver;
+    
+    @ManyToOne
+    @JoinColumn(name = "job_id", nullable = true)
+    private Job job;
+    
+    @Column(length = 64000)
+    private String text;
+    
+    private String created;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUserSender() {
+		return userSender;
+	}
+
+	public void setUserSender(User userSender) {
+		this.userSender = userSender;
+	}
+
+	public User getUserReceiver() {
+		return userReceiver;
+	}
+
+	public void setUserReceiver(User userReceiver) {
+		this.userReceiver = userReceiver;
+	}
+
+	public Job getJob() {
+		return job;
+	}
+
+	public void setJob(Job job) {
+		this.job = job;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+    
+    
+
+}
